be2c02daf799c1fdc0ab226556f409285a7bc695
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2021-06-06 13:47:45 -0700

    File update

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,18 @@
+require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+
 module.exports = {
   networks: {
+    ropsten:{
+      provider:function(){
+        return new HDWalletProvider(process.env.PRIVATE_KEY, `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`)
+          console.log(provider)
+      },
+      gasPrice:2500000000,
+      network_id:3
+    }
+    ,
     development: {
       host: "127.0.0.1",
       port: 7545,
